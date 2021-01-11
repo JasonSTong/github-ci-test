42d1b4b2dc943c34b89c09d97d2fd1a607604813
ChenCong <820542443@qq.com>
2021-01-11 13:57:42 +0800

    test

---
+++ src/main/java/cn/ha5fun/testgithub/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package cn.ha5fun.testgithub.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author chen
+ * @Company ha5fun.com
+ * @Description 测试
+ * @Date 2021/1/11 1:56 下午
+ * @Version 1.0.0
+ */
+@Controller
+public class TestController {
+    @GetMapping("Hello")
+    public String testHello(){
+        return "hello";
+    }
+}
